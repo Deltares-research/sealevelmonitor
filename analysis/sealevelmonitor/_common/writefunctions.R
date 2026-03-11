e08a25a7d4f66666fe8e3dde12495033f10cd2b5
Willem Stolte <willem.stolte@deltares.nl>
2026-03-11 15:22:07 +0100

    chapter names

--- analysis/sealevelmonitor/_common/writefunctions.R
+++ analysis/sealevelmonitor/_common/writefunctions.R
@@ -2,15 +2,20 @@
 write_aug_long <- function(df, filename){
   
 metadata <- c(
+"# Sealevelmonitor output file",
+"# produced in script sealevelmonitor.Rmd",
+paste("# date produced: ", lubridate::today()),
+"#",
+"# column names explanation",
 "# year = year",
 "# psmsl_id = ID of PSMSL station",
 "# name = station name",
-"# height = measured water level relative to NAP-2005",
-"# surge_anomaly = yearly average surge normalized by the average surge",
-"# surge = yearly average surge",
+"# height = measured water level in mm relative to NAP-2005",
+"# surge_anomaly = yearly average surge in mm normalized by the average surge",
+"# surge = yearly average surge in mm",
 "# from1993 = term to be used in broken linear regression",
 "# from1960_square = term to be used in broken quadratic regression",
-"# offset(surge_anomaly) = surge anomaly used in regression (equals surge_anomaly)",
+"# offset(surge_anomaly) = surge anomaly in mm used in regression (equals surge_anomaly)",
 "# I(year - epoch) = year - 1970",
 "# I(cos(2 * pi * (year - epoch)/(18.613))) = cosine term to estimate nodal tide",
 "# I(sin(2 * pi * (year - epoch)/(18.613))) = sine term to estimate nodal tide",
@@ -23,10 +28,12 @@ metadata <- c(
 "# nodal_lwr = lower value of 95% CI for nodal_fit",
 "# nodal_upr = upper value of 95% CI for nodal_fit",
 "# offset = ",
-"# fitted_total = fitted trend + fitted nodal + surge_anomaly + offset"
+"# fitted_total = fitted trend + fitted nodal + surge_anomaly + offset",
+"#"
 )
 
 write_lines(metadata, filename) 
-write_delim(df, filename, append = TRUE)
+write_delim(df, filename, col_names = T, append = TRUE)
 }
 
+ 
