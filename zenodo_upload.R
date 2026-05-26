31d1abb1fc280a971c54ec59d55cf798946b6d11
Willem Stolte <willem.stolte@deltares.nl>
2026-05-26 09:12:02 +0200

    identity information added

--- zenodo_upload.R
+++ zenodo_upload.R
@@ -8,20 +8,23 @@ zenodo_url <- "https://zenodo.org/api/deposit/depositions"
 
 # Metadata for your upload
 metadata <- list(
-  metadata = list(
-    title = "My Dataset Title",
-    upload_type = "dataset",
-    description = "Description of the dataset.",
-    creators = list(list(name = "Doe, John", affiliation = "University X"))
-  )
+  metadata = list(
+    title = "Dutch Sea Level Monitor",
+    upload_type = "dataset",
+    description = "Data and scripts for the yearly Dutch Sea Level Monitor",
+    creators = list(
+      list(name = "Stolte, Willem", affiliation = "Deltares"),
+      list(name = "Dees, Nathalie", affiliation = "Deltares")
+    )
+  )
 )
 
 # Create a new deposition
 res <- POST(
-  zenodo_url,
-  add_headers(Authorization = paste("Bearer", zenodo_token)),
-  body = metadata,
-  encode = "json"
+  zenodo_url,
+  add_headers(Authorization = paste("Bearer", zenodo_token)),
+  body = metadata,
+  encode = "json"
 )
 
 deposition <- content(res)
@@ -31,13 +34,13 @@ bucket_url <- deposition$links$bucket
 file_path <- "data/my_dataset.csv"
 file_name <- basename(file_path)
 PUT(
-  url = paste0(bucket_url, "/", file_name),
-  add_headers(Authorization = paste("Bearer", zenodo_token)),
-  body = upload_file(file_path)
+  url = paste0(bucket_url, "/", file_name),
+  add_headers(Authorization = paste("Bearer", zenodo_token)),
+  body = upload_file(file_path)
 )
 
 # Publish the deposition
 POST(
-  url = paste0(zenodo_url, "/", deposition$id, "/actions/publish"),
-  add_headers(Authorization = paste("Bearer", zenodo_token))
+  url = paste0(zenodo_url, "/", deposition$id, "/actions/publish"),
+  add_headers(Authorization = paste("Bearer", zenodo_token))
 )
